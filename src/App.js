8aab564892c14af04d2fd67458be4ebf29ac79ae
eugene shuliakivsky <zimishe@gmail.com>
2017-07-17 19:18:53 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import postsData from './data/posts.json'
+import usersData from './data/users.json'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            users: 1,
+            posts: '228'
+        }
+    }
+    
+    setUser = (user) => {
+        this.setState(() => {
+            return {users: user}
+        })
+    };
+    
+    
+    render() {
+        
+        
+        return (
+            <div className="posts">
+                <h1>Posts{this.state.users}</h1>
+                
+                <nav>
+                    <label>City filter:
+                        <select onChange={this.setUser.bind(this, 3000)}>
+                            <option>100</option>
+                            <option>200</option>
+                        </select>
+                    </label>
+                    <label>Company filter: </label>
+                    <label>Quick search by post title</label>
+                    <hr/>
+                    <label>Sort by: </label>
+                </nav>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
